83a0eaca3d7183c53cefbfe3ac2c17494db22ba9
Eduardo Santos <santos.eduardothomaz@gmail.com>
2021-07-11 14:56:13 -0300

    :heavy_check_mark: Implemented testing to fetch data from the web

--- test/github_issues_test.exs
+++ test/github_issues_test.exs
@@ -1,18 +1,16 @@
 defmodule GithubIssuesTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Issues
 
   import Issues.GithubIssues, only: [fetch: 2]
 
   test "Check if connection is established" do
-    res = fetch("eduardothsantos", "github-issues")
-    assert res[:status] == :ok
+    res = fetch("elixir-lang", "elixir")
     assert res[:status_code] == 200
   end
 
   test "Checking error 404: not found" do
     res = fetch("eduardothsantos", "asdjasdjlasdaaslj")
-    assert res[:status] == :error
     assert res[:status_code] == 404
   end
 end
