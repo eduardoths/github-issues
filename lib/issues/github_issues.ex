017692e01869bfb4b8c836d98decc73a549d4980
Eduardo Santos <santos.eduardothomaz@gmail.com>
2021-07-11 15:18:22 -0300

    :sparkles: Implemented logger

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,10 +1,14 @@
 defmodule Issues.GithubIssues do
 
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir santos.eduardothomaz@gmail.com"}]
   @github_url Application.get_env(:issues, :github_url)
 
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     get_user_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -17,6 +21,8 @@ defmodule Issues.GithubIssues do
 
 
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
     %{status_code: status_code,
       body: Poison.Parser.parse!(body)}
   end
