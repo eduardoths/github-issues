1bbdc904250634c46923d00f5a7ca4a1e5d4bbfe
Eduardo Santos <santos.eduardothomaz@gmail.com>
2021-07-11 14:55:29 -0300

    :heavy_check_mark: Implemented cli application testing

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,8 +1,8 @@
 defmodule CliTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Issues
 
-  import Issues.CLI, only: [parse_args: 1]
+  import Issues.CLI, only: [parse_args: 1, sort_descending: 1]
 
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h",         "anything"]) == :help
@@ -15,5 +15,13 @@ defmodule CliTest do
 
   test "count is defaulted if only two values were given" do
     assert parse_args(["user", "projecto"]) == {"user", "projecto", 4}
+  end
+
+  test "sort descending order in the correct way" do
+    fake_list = for value <- ["a", "z", "c"],
+      do: %{"created_at" => value,"unused_data" => "a"}
+    result = sort_descending(fake_list)
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{ z c a }
   end
 end
